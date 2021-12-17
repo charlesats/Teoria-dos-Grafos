f5ff93470b1bd8ec1fed5cc00f169f8ada54264d
Charles Assis <charles.torres@aluno.ufop.edu.br>
2021-12-16 23:40:58 -0300

    Update main.py

--- Trabalho_01/main.py
+++ Trabalho_01/main.py
@@ -1,31 +1,12 @@
-def converte_matriz(L, v):
-    matriz = [[0 for _ in range(v)] for _ in range(v)]
-    return matriz
 
-
-
-L = []
-M = []
-T = []
 with open('example.txt') as arquivo:
+    M = {}
     for linha in arquivo:
-        L.append(linha.strip().split())
-
-for i in range(0, len(L)):
-    for j in range(0, len(L[i])):
-        L[i][j] = int(L[i][j])
-
-vertices = L[0][0]
-arestas = L[0][1]
+        u, v = map(int, linha.split())
+        if u not in M:
+            M[u] = [v]
+        else:
+            M[u].append(v)
 
 
-for i in range(1, len(L)):
-    M.append(L[i])
-
-for i in range(len(M)):
-    T.append(M[i][0])
-
-#print(vertices)
-#print(arestas)
 print(M)
-print(converte_matriz(M, vertices))
